60b2cf111a2650ba479ba0405026af16a15448e9
PinheiroLG <gustavo.paespinheiro@gmail.com>
2023-01-14 16:05:13 -0300

    Adicionado a logo correta do Instagram e criando a estrutura inicial da página.

--- loginInstagram/App.js
+++ loginInstagram/App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='#FFF' transLucent={false} />
+
+      <Image source={require('./src/assets/logoinstagram.png')} style={styles.logo}/>
+
+      <TextInput
+        placeholder="Celular, username ou email"
+        styles={styles.input}
+      />
+      <TextInput
+        placeholder="Sua senha"
+        styles={styles.input}
+      />
+      <View style={styles.forgotContainer}>
+        <TouchableOpacity>
+          <Text style={styles.forgotText}>Esqueceu sua senha?
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginText}>Acessar</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
